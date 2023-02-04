6934f220065dd90fec3ea3e9215331555dd63bcd
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-02-04 16:34:36 +0700

    chore(Services): avoid gas clouds & asteroids

--- src/main/java/Services/BotService.java
+++ src/main/java/Services/BotService.java
@@ -34,17 +34,55 @@ public class BotService {
     }
 
     public void computeNextPlayerAction(PlayerAction playerAction) {
-        playerAction.action = PlayerActions.FORWARD;
-        playerAction.heading = new Random().nextInt(360);
 
         if (!gameState.getGameObjects().isEmpty()) {
+
+            // Game Tick Payload
+            var gameObjects = gameState.getGameObjects();
+            var playerGameObjects = gameState.getPlayerGameObjects();
+            var worldObjects = gameState.getWorld();
             var foodList = gameState.getGameObjects()
                     .stream().filter(item -> item.getGameObjectType() == ObjectTypes.FOOD)
                     .sorted(Comparator
                             .comparing(item -> getDistanceBetween(bot, item)))
                     .collect(Collectors.toList());
 
-            playerAction.heading = getHeadingBetween(foodList.get(0));
+            var playerList = gameState.getPlayerGameObjects()
+                    .stream().filter(item -> item.getGameObjectType() == ObjectTypes.PLAYER)
+                    .sorted(Comparator.comparing(item -> getDistanceBetween(bot, item)))
+                    .collect(Collectors.toList());
+
+
+            final int MAX_GAP_WITH_BOUNDARY = 150;
+            final int MAX_GAP_WITH_OTHERSHIPS = 50;
+            final int MAX_GAP_WITH_HARMFUL_OBJECTS = 25;
+            double gap = getDistanceBetween(this.bot, worldObjects);
+            double nearestShips = getDistanceBetween(this.bot, playerList.get(0));
+            playerAction.action = PlayerActions.FORWARD;
+            if (gap <= MAX_GAP_WITH_BOUNDARY) {
+                // move towards center
+                playerAction.heading = getHeadingBetween();
+            }
+            else {
+                if (nearestShips <= MAX_GAP_WITH_OTHERSHIPS) {
+                    int sizeDiff = playerList.get(0).getSize() - this.bot.getSize();
+                    if (sizeDiff > 20) {
+                        // move to opposite direction
+                        playerAction.heading = (-1) * getHeadingBetween(playerList.get(0));
+                    }
+                    else if (sizeDiff < 20 && sizeDiff >= -20) {
+                        playerAction.heading = getHeadingBetween(foodList.get(0));
+                    }
+                    else {
+                        // pursue small ship
+                        playerAction.heading = getHeadingBetween(playerList.get(0));
+                    }
+                }
+                else {
+                    playerAction.heading = getHeadingBetween(foodList.get(0));
+                }
+            }
+
         }
 
         this.playerAction = playerAction;
@@ -70,9 +108,20 @@ public class BotService {
         return Math.sqrt(triangleX * triangleX + triangleY * triangleY);
     }
 
+    private double getDistanceBetween(GameObject bot, World world) {
+        var botPosition = Math.sqrt(bot.getPosition().x * bot.getPosition().x + bot.getPosition().y * bot.getPosition().y);
+        return world.getRadius() - botPosition;
+    }
+
     private int getHeadingBetween(GameObject otherObject) {
         var direction = toDegrees(Math.atan2(otherObject.getPosition().y - bot.getPosition().y,
                 otherObject.getPosition().x - bot.getPosition().x));
+        return (direction + 360) % 360;
+    }
+
+    private int getHeadingBetween() {
+        var direction = toDegrees(Math.atan2(-bot.getPosition().y,
+                -bot.getPosition().x));
         return (direction + 360) % 360;
     }
 
